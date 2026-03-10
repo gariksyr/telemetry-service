a935a84616bf05b2513ba20c8b92d1ef064d4617
Игорь <gariksyr@gmail.com>
2026-03-10 22:18:54 +0300

    message for different exceptions

--- src/main/java/com/thesis/telemetry_service/dto/MeasurementRequestDTO.java
+++ src/main/java/com/thesis/telemetry_service/dto/MeasurementRequestDTO.java
@@ -2,12 +2,16 @@ package com.thesis.telemetry_service.dto;
 
 import com.thesis.telemetry_service.util.ValidImo;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class MeasurementRequestDTO {
-    @ValidImo
+    @ValidImo(message = "invalid IMO")
     private String vesselImo;
+    @Range(min = -90, max = 90, message = "invalid latitude")
     private Double latitude;
+    @Range(min = -180, max = 180, message = "invalid longitude")
     private Double longitude;
+    @Range(min = 0, max = 70, message = "unacceptable speed")
     private Double speed;
 }
