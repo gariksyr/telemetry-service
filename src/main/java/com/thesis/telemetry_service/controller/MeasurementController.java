8db0ec65e13a966b4d8be2bca95a9efd3e0e231f
Игорь <gariksyr@gmail.com>
2026-03-15 21:33:07 +0300

    add swagger description

--- src/main/java/com/thesis/telemetry_service/controller/MeasurementController.java
+++ src/main/java/com/thesis/telemetry_service/controller/MeasurementController.java
@@ -3,37 +3,44 @@ package com.thesis.telemetry_service.controller;
 import com.thesis.telemetry_service.dto.MeasurementRequestDTO;
 import com.thesis.telemetry_service.dto.MeasurementResponseDTO;
 import com.thesis.telemetry_service.service.MeasurementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Tag(name = "Measurement management")
 @RestController
 @RequestMapping("/api/v1/telemetry")
 @RequiredArgsConstructor
 public class MeasurementController {
     private final MeasurementService measurementService;
+    @Operation(description = "Add new measurement")
     @PostMapping()
     public ResponseEntity<MeasurementResponseDTO> addMeasurement(@Valid @RequestBody  MeasurementRequestDTO measurement){
         return ResponseEntity.ok(measurementService.addMeasurement(measurement));
     }
+    @Operation(description = "Get all measurements")
     @GetMapping
     public ResponseEntity<Page<MeasurementResponseDTO>> findAll(@RequestParam(defaultValue = "0")  Integer page, @RequestParam(defaultValue = "20") Integer size){
         return ResponseEntity.ok(measurementService.findAll(page, size));
     }
+    @Operation(description = "Find measurements by required IMO")
     @GetMapping("/{imo}")
     public ResponseEntity<Page<MeasurementResponseDTO>> findMeasures(@PathVariable("imo") String imo,
                                                                      @RequestParam(value = "page", defaultValue = "0") Integer page,
                                                                      @RequestParam(value = "size", defaultValue = "20") Integer size){
         return ResponseEntity.ok(measurementService.findByImo(imo, page, size));
     }
+    @Operation(description = "Delete measurement by id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMeasurement(@PathVariable("id") Long id){
         measurementService.deleteMeasurement(id);
         return ResponseEntity.ok().build();
     }
+    @Operation(description = "Get all near measurements from required vessel IMO by radius, latitude and longitude")
     @GetMapping("/{imo}/near")
     public ResponseEntity<Page<MeasurementResponseDTO>> getNear(
             @PathVariable String imo,
@@ -44,6 +51,7 @@ public class MeasurementController {
             @RequestParam(defaultValue = "1000") Double radius) {
         return ResponseEntity.ok(measurementService.findNear(page, size, imo, lat, lon, radius));
     }
+    @Operation(description = "Get latest measurements from all vessels")
     @GetMapping("/latest")
     public ResponseEntity<Page<MeasurementResponseDTO>> getLatest(
             @RequestParam(defaultValue = "0") int page,
--- src/main/java/com/thesis/telemetry_service/dto/MeasurementRequestDTO.java
+++ src/main/java/com/thesis/telemetry_service/dto/MeasurementRequestDTO.java
@@ -1,17 +1,22 @@
 package com.thesis.telemetry_service.dto;
 
 import com.thesis.telemetry_service.util.ValidImo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
 public class MeasurementRequestDTO {
+    @Schema(description = "Unique 7-digit vessel code", example = "9311622", requiredMode = Schema.RequiredMode.REQUIRED)
     @ValidImo(message = "invalid IMO")
     private String vesselImo;
+    @Schema(description = "Latitude (WGS 84)", example = "59.9311", minimum = "-90", maximum = "90")
     @Range(min = -90, max = 90, message = "invalid latitude")
     private Double latitude;
+    @Schema(description = "Longitude (WGS 84)", example = "30.3609", minimum = "-180", maximum = "180")
     @Range(min = -180, max = 180, message = "invalid longitude")
     private Double longitude;
+    @Schema(description = "Vessel speed in knots", example = "12.5", minimum = "0", maximum = "70")
     @Range(min = 0, max = 70, message = "unacceptable speed")
     private Double speed;
 }
